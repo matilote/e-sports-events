5fa9b0ac24343fde86820c26307a200cdbecf18d
Mateusz J <mateusz.jedrzejewski@yahoo.pl>
2019-03-24 16:11:47 +0100

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layouts/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let render = () => {
+    ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+if(module.hot) {
+    module.hot.accept('./app/layouts/App.tsx', () => {
+        setTimeout(render)
+    })
+}
+
+render();
+
 serviceWorker.unregister();
